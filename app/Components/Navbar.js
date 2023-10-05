0291c0f28525f525169d9ef6242358cd16149458
karm1000 <karmdsoni8159@gmail.com>
2023-10-05 10:22:40 +0530

    Transition To NextJS

---
+++ app/Components/Navbar.js
@@ -0,0 +1,53 @@
+"use client";
+import React, { useState } from "react";
+import styles from "../styles/navbar.module.css";
+import Image from "next/image";
+// import logo from "@/public/.svg";
+import Link from "next/link";
+
+
+const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const searchHappen = (e)=>{
+    setSearchQuery(e.target.value);
+    console.log(searchQuery);
+  }
+
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <div className={styles.logo}>
+            <Link href={"/"}>
+                <Image src="/Academia.svg" alt="acad" width={100} height={30} priority />
+            </Link>
+        </div>
+        {/* <div className={styles.links}>
+            <Link href={"/"}>Explore</Link>
+            <Link href={"/"}>Quiz and Practice</Link>
+        </div> */}
+        
+        <div className={styles.searchBar}>
+            <div>
+                <input type="text" placeholder="Search Acedemia" onChange={searchHappen}/>
+                <button><Image src={"/searchicon.svg"} alt="search" width={26} height={26} priority/></button>
+            </div>
+        </div>
+        <div className={styles.login}>
+          <button> Login </button>
+        </div>
+
+        <div className={styles.profile}>
+            <div>
+                <Link href={"/profile"}>
+                    <Image src={"/favicon.ico"} alt="acad" width={34} height={34} priority/>
+                </Link>
+            </div>
+        </div>
+        
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ app/Components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/sidebar.module.css'
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+        I am Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
